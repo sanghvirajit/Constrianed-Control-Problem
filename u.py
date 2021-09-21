c7d668ab819c0060284e46d97707d05b8f5a093a
sanghvirajit <rajit.msanghvi@gmail.com>
2021-09-21 12:00:50 +0200

    Control-Constrained-Problem added

---
+++ u.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+u = pd.read_csv("u.txt", header=None)
+df = pd.DataFrame(index=np.arange((u.size)/31), columns=np.arange(1))
+
+n = 15
+
+for i in range(0, 129):
+	df.iloc[i, 0] = u.iloc[i*31 + n, 0]
+	
+df.to_csv("u_final.txt", header=None, index=False)
